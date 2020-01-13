fca695d0d38b7f525ed61c95f7f25329b04fb0ea
Axel Rock <rockaxel@gmail.com>
2020-01-13 17:54:31 +0100

    Bug fixes: - demo function called on the right element

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ TweenMax.to('#regular-gsap>*', 5, {scale: 1.1})
 var element = document.querySelector('#canvas-kenburns>*')
 new KenBurns(element)
 
-kenburns.scaleTo(1.1, 5)
-// kenburns.scaleFrom(1.1, 5)
+element.scaleTo(1.1, 5)
+// element.scaleFrom(1.1, 5)
